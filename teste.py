cfa2afd1d979e509efa93f939daff69276da24a8
Gabriel Travaini <gabrieltravaini.8039@aluno.saojudas.br>
2022-06-09 21:29:10 -0300

    created a word countes

--- teste.py
+++ teste.py
@@ -1,1 +1,25 @@
-print('ISSO Ë um teste, quem baixar é babaca')
+
+import os
+import datetime as dt
+
+os.system('cls')
+
+start_time = dt.datetime.now()
+
+chars = input ('digite o mais rapido que puder! ')
+end_time =  dt.datetime.now()
+
+
+total_time = (end_time - start_time)
+total_time = total_time.total_seconds()
+
+CPS = len(chars)/total_time
+
+wordlist = chars.split(' ')
+total_time_m = total_time/60
+WPS = len(wordlist)/total_time_m
+
+print('---------------------------------------------------------------------')
+print('Você digita '+ str(CPS)+ ' letras por segundo')
+print('Você digita '+ str(WPS)+ ' palavras por minuto')
+print('---------------------------------------------------------------------')
